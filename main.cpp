e11abf77fb99ff7c66200436e41d604023717efa
sheerluck <sheer.luck.andrew@gmail.com>
2021-05-20 23:50:43 +0300

    dots are less verbose

--- main.cpp
+++ main.cpp
@@ -5,18 +5,20 @@ int main()
 {
     auto counter = int_t{0};
     auto i = int_t{5};
-    while (i < 10'000'000'000)
+    while (i < 10'000'000'000'000)
     {
         if (coprime_orders(i))
         {
-            std::cout << rang::fgB::red
+            std::cout << '\n'
+                      << rang::fgB::red
                       << i
                       << rang::fg::reset
-                      << '\n';
+                      << std::flush;
         }
 
         i = mppp::nextprime(i);
 
-        if (0 == (++counter % 1000'000)) std::cout << "test " << i << "\n";
+        if (0 == (++counter % 1000'000))
+            std::cout << '.' << std::flush;
     }
 }
